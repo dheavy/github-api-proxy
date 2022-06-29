e83c999502353c97a27801813f683e76f5791bc0
Davy Peter BRAUN <hello@davybraun.com>
2022-06-29 13:38:29 +0200

    feat: start building Octokit based API search

--- src/controllers/search.ts
+++ src/controllers/search.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from 'express';
 import { MSG_QUERY_ERROR_INSTRUCTIONS } from '../constants';
+import { Octokit } from '@octokit/rest';
 
 export const search = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -14,6 +15,15 @@ export const search = async (req: Request, res: Response): Promise<void> => {
       return;
     }
 
+    // Configure a Github Octokit client.
+    // Set dates on payload to (hopefully) see them in local timezone.
+    const currentTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const auth = process.env.PERSONAL_ACCESS_TOKEN || '';
+    const octokit = new Octokit({
+      auth,
+      timeZone: currentTimeZone
+    });
+
     res.status(200).json({
       data: {
         users: [],
@@ -27,6 +37,6 @@ export const search = async (req: Request, res: Response): Promise<void> => {
     res.send({
       data: null,
       errors: ['oops!']
-    })
+    });
   }
 }
